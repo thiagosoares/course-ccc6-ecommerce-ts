8d2afd8ffc20463ad1b39ee8739857c2dabe0a30
CIANDT\thiago.soares <thiago.soares@ciandt.com>
2022-04-20 20:06:42 -0300

    2 - Simplify and organize IFs

--- src/CpfValidator.ts
+++ src/CpfValidator.ts
@@ -1,48 +1,46 @@
 
 export function validateCpf (cpf?: string | null) {
 
-	if (cpf !== null) {
-        if (cpf !== undefined) {
-            if (cpf.length >= 11 && cpf.length <= 14) {  // FIXME Condição zoada. Permite valores com size 1 por exemplo
+    if (cpf == null) {
+        return false
+    }
+    if (cpf.length < 11 && cpf.length > 14) {
+        return false
+    }
 
-                cpf = cpf
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");
+    cpf = cpf
+        .replace('.','')
+        .replace('.','')
+        .replace('-','')
+        .replace(" ","");
 
-                if (!cpf.split("").every(c => c === cpf?.substring(0,1))) { // FIXME Condição para evitar cpfs como 111.111.111-11
+    if (cpf.split("").every(c => c === cpf?.substring(0,1))) { // TODO FUNC Condição para evitar cpfs como 111.111.111-11
+        return false
+    }
 
-                    let sumFirstCheckDigit = 0
-                    let sumSecondCheckDigit = 0
-                    let firstCheckDigit, secondCheckDigit
-                    let divisionRest
+    let sumFirstCheckDigit = 0
+    let sumSecondCheckDigit = 0
+    let firstCheckDigit, secondCheckDigit
+    let divisionRest
 
-                    // For into each number on CPF - Make Sum
-                    for (let nCount = 1; nCount < cpf.length -1; nCount++) {  // TODO For confurso. Não Percorre até os Digitos
-                        let digito = parseInt(cpf.substring(nCount -1, nCount));
-                        sumFirstCheckDigit = sumFirstCheckDigit + ( 11 - nCount ) * digito;
-                        sumSecondCheckDigit = sumSecondCheckDigit + ( 12 - nCount ) * digito;
-                    }
+    // For into each number on CPF - Make Sum
+    for (let nCount = 1; nCount < cpf.length -1; nCount++) {  // TODO For confurso. Não Percorre até os Digitos
+        let digito = parseInt(cpf.substring(nCount -1, nCount));
+        sumFirstCheckDigit = sumFirstCheckDigit + ( 11 - nCount ) * digito;
+        sumSecondCheckDigit = sumSecondCheckDigit + ( 12 - nCount ) * digito;
+    }
 
-                    divisionRest = (sumFirstCheckDigit % 11);
+    divisionRest = (sumFirstCheckDigit % 11);
 
-                    firstCheckDigit = (divisionRest < 2) ? 0 : 11 - divisionRest;
-                    sumSecondCheckDigit += 2 * firstCheckDigit;
+    firstCheckDigit = (divisionRest < 2) ? 0 : 11 - divisionRest;
+    sumSecondCheckDigit += 2 * firstCheckDigit;
 
-                    divisionRest = (sumSecondCheckDigit % 11);
-                    if (divisionRest < 2) {  //  TODO If sem {}. Que droga
-                        secondCheckDigit = 0;
-                    } else {
-                        secondCheckDigit = 11 - divisionRest;
-                    }
+    divisionRest = (sumSecondCheckDigit % 11);
 
-                    let originalCheckDigit = cpf.substring(cpf.length-2, cpf.length);
-                    let calculatedCheckDigiti = "" + firstCheckDigit + "" + secondCheckDigit;
-                    return originalCheckDigit == calculatedCheckDigiti; // TODO Nome de Variavel
+    secondCheckDigit = divisionRest < 2 ? 0 : 11 - divisionRest
 
-                } else return false
-            } else return false // FIXME Necessário arrumar o IF para cair no else
-        } else return false
-	} else return false
+    let originalCheckDigit = cpf.substring(cpf.length-2, cpf.length);
+    let calculatedCheckDigit = "" + firstCheckDigit + "" + secondCheckDigit;
+
+    return originalCheckDigit == calculatedCheckDigit; // TODO Nome de Variavel
 }
